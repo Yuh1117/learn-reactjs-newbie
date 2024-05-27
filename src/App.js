d1c647ca533c9741f1e846c24170999dc3bfcd81
Yuh <huypham3188@gmail.com>
2024-05-27 14:06:46 +0700

    nested-routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.scss';
 import Header from './components/Header/Header';
-import { Link } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        <button>
-          <Link to="/users">user page</Link>
-        </button>
-        <button>
-          <Link to="/admins">admin page</Link>
-        </button>
+      <div className='header-container'>
+        <Header />
       </div>
+      <div className='main-container'>
+        <div className='sidebar-container'>
+
+        </div>
+        <div className='app-content'>
+          <Outlet />
+        </div>
+      </div>
+
     </div>
   );
 }
